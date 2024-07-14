76bc2c847e1dcaae00f9ea76355f49858539b92a
Vanshika Verma <vanshika.j.verma@gmail.com>
2024-07-15 01:15:26 +0530

    updated

--- app.py
+++ app.py
@@ -5,14 +5,16 @@ st.title('🦜🔗 Quickstart App')
 
 openai_api_key = st.sidebar.text_input('OpenAI API Key')
 
-def generate_response(input_text):
-  llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
+def generate_response(input_text, api_key):
+  llm = OpenAI(temperature=0.7, openai_api_key=api_key)
   st.info(llm(input_text))
 
 with st.form('my_form'):
   text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
   submitted = st.form_submit_button('Submit')
-  if not openai_api_key.startswith('sk-'):
-    st.warning('Please enter your OpenAI API key!', icon='⚠')
-  if submitted and openai_api_key.startswith('sk-'):
-    generate_response(text)
+  
+  if submitted:
+    if not openai_api_key.startswith('sk-'):
+      st.warning('Please enter a valid OpenAI API key!', icon='⚠')
+    else:
+      generate_response(text, openai_api_key)
